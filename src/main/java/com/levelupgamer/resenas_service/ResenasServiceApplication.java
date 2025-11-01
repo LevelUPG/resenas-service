4372e38fe19b7f07ca1245bb9ad103f7e846ee34
alejandro <alejandrobricenobenavides@gmail.com>
2025-11-01 19:25:00 -0300

    first commit

---
+++ src/main/java/com/levelupgamer/resenas_service/ResenasServiceApplication.java
@@ -0,0 +1,13 @@
+package com.levelupgamer.resenas_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResenasServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResenasServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/levelupgamer/resenas_service/ResenasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelupgamer.resenas_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResenasServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
